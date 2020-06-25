427cc9b3b941026659aa16a57b83ddb1e4209383
Maxime404 <galissairemaxime24@gmail.com>
2020-06-25 15:53:43 +0200

    WIP - Fixed few maps but not everyone

--- .dist/src/Maper.js
+++ .dist/src/Maper.js
@@ -2,12 +2,57 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class Maper {
     constructor(map) {
+        this.linesContent = [];
+        this.mapLength = 0;
+        this.mapHeight = 0;
+        this.x = 0;
+        this.y = 0;
+        this.knownLocations = [[0, 0]];
         this.map = map;
     }
     solveMap() {
-        const linesContent = this.map.split('\n');
-        const mapfile = linesContent[0].length;
+        this.linesContent = this.map.split('\n');
+        if (this.linesContent[0].includes('x')) {
+            const sizeInfos = this.linesContent[0].trim().split('x');
+            console.log(sizeInfos);
+            this.mapLength = +sizeInfos[0];
+            this.mapHeight = +sizeInfos[1];
+            this.y += 1;
+        }
+        else {
+            this.mapHeight = this.linesContent.length;
+            this.mapLength = this.linesContent[0].length;
+        }
+        while (this.linesContent[this.y].indexOf('1') === -1) {
+            this.y += 1;
+        }
+        const startPoint = this.linesContent[this.y].indexOf('1');
+        let loop = 0;
+        this.x = startPoint;
+        this.y = 1;
+        while (this.linesContent[this.y].charAt(this.x) !== '2' && loop < this.mapHeight) {
+            console.log(this.linesContent[this.y].charAt(this.x));
+            this.move();
+            loop + 1;
+        }
+        console.log(`End point found at [${this.x}, ${this.y}] !`);
         return true;
+    }
+    move() {
+        if (this.y + 1 < this.mapHeight && this.linesContent[this.y + 1].charAt(this.x) !== '*') {
+            this.y += 1;
+        }
+        else if (this.y + 1 < this.mapHeight && this.x + 1 < this.mapLength && this.linesContent[this.y + 1].charAt(this.x + 1) !== '*' && !this.isKnownLocation(this.y + 1, this.x + 1)) {
+            this.y += 1;
+            this.x += 1;
+        }
+        else if (this.linesContent[this.y + 1].charAt(this.x--) !== '*') {
+            this.y += 1;
+            this.x -= 1;
+        }
+    }
+    isKnownLocation(x, y) {
+        return JSON.stringify(this.knownLocations).indexOf(JSON.stringify([x, y])) !== -1 ? true : false;
     }
 }
 exports.default = Maper;
--- src/Maper.ts
+++ src/Maper.ts
@@ -7,7 +7,7 @@ export default class Maper {
     private mapHeight: number = 0;
     private x: number = 0;
     private y: number = 0;
-    private knownLocations: Array<[number, number]> = [[0, 0]];
+    private knownLocations: Array<[number, number]> = [];
 
     constructor(map: string) {
         this.map = map;
@@ -15,7 +15,7 @@ export default class Maper {
 
     solveMap(): boolean {
         this.linesContent = this.map.split('\n');
-        if(this.linesContent[0].includes('x')) {
+        if (this.linesContent[0].includes('x')) {
             const sizeInfos: Array<string> = this.linesContent[0].trim().split('x');
             console.log(sizeInfos)
             this.mapLength = +sizeInfos[0];
@@ -25,7 +25,7 @@ export default class Maper {
             this.mapHeight = this.linesContent.length;
             this.mapLength = this.linesContent[0].length;
         }
-        while(this.linesContent[this.y].indexOf('1') === -1) {
+        while (this.linesContent[this.y].indexOf('1') === -1) {
             this.y += 1;
         }
 
@@ -33,30 +33,64 @@ export default class Maper {
         let loop: number = 0;
 
         this.x = startPoint;
-        this.y = 1;
+        this.knownLocations.push([this.x, this.y]);
 
-        while (this.linesContent[this.y].charAt(this.x) !== '2' && loop < this.mapHeight) {
+        while (this.linesContent[this.y].charAt(this.x) !== '2' && loop < 200) {
             console.log(this.linesContent[this.y].charAt(this.x));
+            //console.log(this.knownLocations);
             this.move();
-            loop + 1;
+            loop += 1;
         }
         console.log(`End point found at [${this.x}, ${this.y}] !`);
         return true;
     }
 
     private move() {
-        if (this.y + 1 < this.mapHeight && this.linesContent[this.y + 1].charAt(this.x) !== '*') {
+        if (this.y + 1 <= this.mapHeight && this.linesContent[this.y + 1].charAt(this.x) !== '*' && !this.isKnownLocation(this.x, this.y + 1)) {
             this.y += 1;
-        } else if (this.y + 1 < this.mapHeight && this.x + 1 < this.mapLength && this.linesContent[this.y + 1].charAt(this.x + 1) !== '*' && !this.isKnownLocation(this.y + 1, this.x + 1)) {
-            this.y += 1;
+            console.log(`[${this.x}, ${this.y}]`);
+        } else if (this.x + 1 <= this.mapLength && this.linesContent[this.y].charAt(this.x + 1) !== '*' && !this.isKnownLocation(this.x + 1, this.y)) {
             this.x += 1;
-        } else if (this.linesContent[this.y + 1].charAt(this.x--) !== '*') {
-            this.y += 1;
+            console.log(`[${this.x}, ${this.y}]`);
+        } else if (this.x - 1 >= 0 && this.linesContent[this.y].charAt(this.x - 1) !== '*' && !this.isKnownLocation(this.x - 1, this.y)) {
             this.x -= 1;
-        }
+            console.log(`[${this.x}, ${this.y}]`);
+        } else if (this.y - 1 >= 0) {
+            if (this.linesContent[this.y].charAt(this.x + 1) !== '*') {
+                while (this.isKnownLocation(this.x, this.y) && this.linesContent[this.y].charAt(this.x + 1) !== '*') {
+                    this.x += 1;
+                    this.addKnownLocation(this.x, this.y);
+                    console.log(`[${this.x}, ${this.y}]`);
+                }
+                if(this.linesContent[this.y - 1].charAt(this.x) !== '*') {
+                    this.y -= 1;
+                    this.addKnownLocation(this.x, this.y);
+                    console.log(`[${this.x}, ${this.y}]`);
+                }
+            } else if (this.linesContent[this.y].charAt(this.x - 1) !== '*') {
+                while (this.isKnownLocation(this.x, this.y) && this.linesContent[this.y].charAt(this.x - 1) !== '*') {
+                    this.x -= 1;
+                    this.addKnownLocation(this.x, this.y);
+                    console.log(`[${this.x}, ${this.y}]`);
+                }
+                if(this.linesContent[this.y - 1].charAt(this.x) !== '*') {
+                    this.y -= 1;
+                    this.addKnownLocation(this.x, this.y);
+                    console.log(`[${this.x}, ${this.y}]`);
+                }
+            }
+        } 
+
+        this.addKnownLocation(this.x, this.y);
     }
 
-    private isKnownLocation(x: number, y: number): boolean {        
+    private isKnownLocation(x: number, y: number): boolean {
         return JSON.stringify(this.knownLocations).indexOf(JSON.stringify([x, y])) !== -1 ? true : false;
+    }
+
+    private addKnownLocation(x: number, y: number) {
+        if (!this.isKnownLocation(this.x, this.y)) {
+            this.knownLocations.push([this.x, this.y]);
+        }
     }
 }
